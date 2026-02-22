80861e18dab76451fc90f4478d7f078c48377dc8
khalid-webdev <khaleduzzaman67@gmail.com>
2026-02-22 06:36:12 +0600

    creating new category post request

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const mongoose = require('mongoose')
 const express = require("express");
 const app = express();
 const userRoutes = require("./routes/userRoutes");
-const authRoutes = require("./routes/authRoutes")
+const authRoutes = require("./routes/authRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 
 mongoose.connect('mongodb://localhost:27017/user-db').then(() => console.log('MongoDb connected successfully.....')).catch((err) => console.log('Database Connection Failed!!!', err))
 
 app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/category", categoryRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server is listening on localhost:${PORT}......`))
---
+++ routes/categoryRoutes.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const multer = require("multer");
+const Category = require("../models/categoryModel");
+const router = express.Router();
+
+/* -------------------------------- category post requests ------------------------------- */
+//* upload category icon and name with multer
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "upload/category");
+  },
+  filename: (req, file, cb) => {
+    const timeStamp = Date.now();
+    const originalName = file.originalname.replace(/\s+/g, "-").replace(/[^a-zA-Z0-9.-]/g);
+    cb(null, `${timeStamp}-${originalName}`);
+  }
+});
+const fileFilter = (req,file,cb) => {
+  const allowFiles = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'];
+  if (allowFiles.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only jpg,jpeg,png and gif are allowed!"), false)
+  }
+}
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limit: {
+    fileSize: 2 * 1024 * 1024
+  }
+});
+router.post("/", upload.single('icon'), async (req, res) => {
+  if (!req.body.name || !req.file) {
+    return res.status(400).json({ message: "Name and icon are missing!!" });
+  }
+  const category = await Category.findOne({ name: req.body.name });
+  if(category)return res.status(400).json({message:"Name is Already exist!!"})
+  const newCategory = new Category({
+    name: req.body.name,
+    image: req.file.filename
+  });
+  await newCategory.save()
+  res.status(201).json(newCategory)
+})
+
+module.exports = router;
