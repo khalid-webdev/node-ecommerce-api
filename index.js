8b3a907f444cf5c6d5c6550a19e121c331932855
khalid-webdev <khaleduzzaman67@gmail.com>
2026-03-10 07:56:54 +0600

    trying to add international payment system but not available in my country

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const authRoutes = require("./routes/authRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 
 mongoose.connect('mongodb://localhost:27017/user-db').then(() => console.log('MongoDb connected successfully.....')).catch((err) => console.log('Database Connection Failed!!!', err))
 
@@ -17,6 +18,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/order",orderRoutes);
+
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`server is listening on localhost:${PORT}......`))
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,7 +1,40 @@
 const express = require("express");
+const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
+const axios = require('axios');
+const {paypal,getAccessToken}=require("../config/paypal");
+
 //creating order for
+router.post("/paypal/create-order",authMiddleware,async(req,res)=>{
+  const accessToken =await getAccessToken()
+  await axios.post(`${paypal.baseUrl}/v2/checkout/orders`,{
+    intent:"CAPTURE",
+    purchase_units:[
+      {
+        description:"Shopping Cart order",
+        amount:{
+          currency_code:"USD",
+          value:10
+        }
+      }
+    ],
+    application_context:{
+      return_url:"",
+      cancel_url:""
+    }
+  },{
+    headers:{
+      "Content-Type":"Application/json",
+      Authorization:`Bearer ${accessToken}`
+    }
+  })
+  res.json({
+    approvalUrl:response.data.links.find((link)=>link.rel==="approve").href
+  })
+});
+
+
 
 
 module.exports = router;
